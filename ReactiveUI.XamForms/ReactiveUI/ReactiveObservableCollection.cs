9ba616f578354939edae1bfcbe1c1fde0847547d
monishi <onikiri2007@gmail.com>
2016-05-16 16:08:16 +1000

    fix bug for reactivelist not binding to list view

---
+++ ReactiveUI.XamForms/ReactiveUI/ReactiveObservableCollection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveUI.XamForms
+{
+    public class ReactiveObservableCollection<T> : ReactiveList<T>
+    {
+        public ReactiveObservableCollection()
+        {
+            this.ItemSource = new ObservableCollection<T>();
+            ItemsAdded.Subscribe(ItemSource.Add);
+            ItemsRemoved.Subscribe((x) => ItemSource.Remove(x));
+            ItemsMoved.Subscribe((x) => ItemSource.Move(x.From, x.To));
+            ItemsMoved.Subscribe((x) => ItemSource.Move(x.From, x.To));
+        }
+
+        public ObservableCollection<T> ItemSource { get; private set; }
+    }
+}
