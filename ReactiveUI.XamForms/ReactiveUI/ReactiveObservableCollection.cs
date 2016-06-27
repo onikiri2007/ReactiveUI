217602b3c078da1ee3aafae67b7e4f1ba808d62e
onikiri2007 <onikiri2007@gmail.com>
2016-06-27 14:51:10 +1000

    reactiveObservable collection support item changed

--- ReactiveUI.XamForms/ReactiveUI/ReactiveObservableCollection.cs
+++ ReactiveUI.XamForms/ReactiveUI/ReactiveObservableCollection.cs
@@ -2,22 +2,33 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive.Disposables;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ReactiveUI.XamForms
 {
-    public class ReactiveObservableCollection<T> : ReactiveList<T>
+    public class ReactiveObservableCollection<T> : ReactiveList<T>, IDisposable
     {
+        private readonly CompositeDisposable disposables = new CompositeDisposable();
         public ReactiveObservableCollection()
         {
             this.ItemSource = new ObservableCollection<T>();
-            ItemsAdded.Subscribe(ItemSource.Add);
-            ItemsRemoved.Subscribe((x) => ItemSource.Remove(x));
-            ItemsMoved.Subscribe((x) => ItemSource.Move(x.From, x.To));
-            ItemsMoved.Subscribe((x) => ItemSource.Move(x.From, x.To));
+            ItemsAdded.Subscribe(ItemSource.Add).DisposeWith(disposables);
+            ItemsRemoved.Subscribe((x) => ItemSource.Remove(x)).DisposeWith(disposables);
+            ItemsMoved.Subscribe((x) => ItemSource.Move(x.From, x.To)).DisposeWith(disposables);
+            ItemChanged.Subscribe(x => {
+                var index = ItemSource.IndexOf(x.Sender);
+                ItemSource[index] = x.Sender;
+            }).DisposeWith(disposables);
         }
 
-        public ObservableCollection<T> ItemSource { get; private set; }
+        public ObservableCollection<T> ItemSource { get; }
+        public void Dispose()
+        {
+            if (disposables.IsDisposed) return;
+            disposables.Dispose();
+            disposables.Clear();
+        }
     }
 }
