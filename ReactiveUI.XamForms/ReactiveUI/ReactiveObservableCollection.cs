d9c3b04784ec2723114222dec8128ec320141e2f
onikiri2007 <onikiri2007@gmail.com>
2016-06-30 10:48:57 +1000

    extend observable collection

--- ReactiveUI.XamForms/ReactiveUI/ReactiveObservableCollection.cs
+++ ReactiveUI.XamForms/ReactiveUI/ReactiveObservableCollection.cs
@@ -1,26 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Disposables;
 using System.Text;
 using System.Threading.Tasks;
+using ReactiveUI;
+using ReactiveUI.XamForms;
 
 namespace ReactiveUI.XamForms
 {
+    public class ObservableCollectionExtended<T> : ObservableCollection<T>
+    {
+        public ObservableCollectionExtended() : base() { }
+
+        public ObservableCollectionExtended(IEnumerable<T> collection) : base(collection) { }
+
+        public ObservableCollectionExtended(List<T> list) : base(list) { }
+
+        public void AddRange(IEnumerable<T> range)
+        {
+            foreach (var item in range)
+            {
+                Items.Add(item);
+            }
+
+            this.OnPropertyChanged(new PropertyChangedEventArgs("Count"));
+            this.OnPropertyChanged(new PropertyChangedEventArgs("Item[]"));
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+
+        }
+
+        public void Reset(IEnumerable<T> range)
+        {
+            this.Items.Clear();
+
+            AddRange(range);
+        }
+    }
+
     public class ReactiveObservableCollection<T> : ReactiveList<T>, IDisposable
     {
         private readonly CompositeDisposable disposables = new CompositeDisposable();
-        public ReactiveObservableCollection()
+        private bool isAddedByRange;
+        private IDisposable itemAddedDisposable;
+        public ReactiveObservableCollection() : this(new ObservableCollectionExtended<T>())
         {
-            this.ItemSource = new ObservableCollection<T>();
-            ItemsAdded.Subscribe(ItemSource.Add).DisposeWith(disposables);
+           
+        }
+
+        public ReactiveObservableCollection(ObservableCollection<T> collection)
+        {
+            this.ItemSource = collection;
+            itemAddedDisposable = ItemsAdded.Subscribe(x => {
+                ItemSource.Add(x);
+
+            }).DisposeWith(disposables);
+
             ItemsRemoved.Subscribe((x) => ItemSource.Remove(x)).DisposeWith(disposables);
             ItemsMoved.Subscribe((x) => ItemSource.Move(x.From, x.To)).DisposeWith(disposables);
             ItemChanged.Subscribe(x => {
                 var index = ItemSource.IndexOf(x.Sender);
                 ItemSource[index] = x.Sender;
             }).DisposeWith(disposables);
+            
         }
 
         public ObservableCollection<T> ItemSource { get; }
@@ -29,6 +74,29 @@ namespace ReactiveUI.XamForms
             if (disposables.IsDisposed) return;
             disposables.Dispose();
             disposables.Clear();
+        }
+
+    
+        public void AddRange(IEnumerable<T> collection, bool bulkUpdate)
+        {
+            var isExtendedList = (this.ItemSource is ObservableCollectionExtended<T>);
+
+            if (isExtendedList && bulkUpdate)
+            {
+                using (itemAddedDisposable)
+                {
+                    (this.ItemSource as ObservableCollectionExtended<T>).AddRange(collection);
+                }
+            }
+
+            base.AddRange(collection);
+
+            if (isExtendedList && bulkUpdate)
+            {
+                itemAddedDisposable = ItemsAdded.Subscribe(x => {
+                    ItemSource.Add(x);
+                });
+            }
         }
     }
 }
